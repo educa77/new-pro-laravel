cc681905a5d9ac038b735db39a743540d7e7b96f
educa77 <eduardo.calo@egoargentina.com>
2021-07-17 20:48:16 -0300

    using eloquent

---
+++ resources/views/cars/create.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="m-auto w-4/8 py-24">
+        <div class="text-center">
+            <h1 class="text-5xl uppercase bold">
+                Create Car
+            </h1>
+            <div class="pt-10">
+                <a href="/cars" class="border-b-2 pb-2 border-dotted italic text-gray-500">
+                   &larr; Back to cars
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-center pt-20">
+        <form action="/cars" method="POST">
+            @csrf
+            <div class="block">
+                <input 
+                type="text"
+                class="block shadow-5xl mb-10 p-2 w-80 italic placeholder-gray-400"
+                name="name"
+                placeholder="Brand name..."
+                >
+                <input 
+                type="text"
+                class="block shadow-5xl mb-10 p-2 w-80 italic placeholder-gray-400"
+                name="founded"
+                placeholder="Founded..."
+                >
+                <input 
+                type="text"
+                class="block shadow-5xl mb-10 p-2 w-80 italic placeholder-gray-400"
+                name="description"
+                placeholder="Description..."
+                >
+                <button 
+                type="submit"
+                class="bg-green-500 block shadow-5xl mb-10 p-2 w-80 uppercase font-bold">
+                    Submit
+                </button>
+            </div>
+        </form>
+    </div>
+@endsection
